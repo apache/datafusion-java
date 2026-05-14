742629c1f42d170fa9c51bfde6a3c203c1a28bdc
Andy Grove <agrove@apache.org>
2026-05-13 18:45:00 -0600

    test(udf): cover concat, square, and repeated invocations

--- core/src/test/java/org/apache/datafusion/ScalarUdfTest.java
+++ core/src/test/java/org/apache/datafusion/ScalarUdfTest.java
@@ -83,4 +83,131 @@ class ScalarUdfTest {
       }
     }
   }
+
+  /** Concatenates two Utf8 columns. */
+  static final class Concat implements ScalarUdf {
+    @Override
+    public FieldVector evaluate(BufferAllocator allocator, List<FieldVector> args) {
+      org.apache.arrow.vector.VarCharVector left = (org.apache.arrow.vector.VarCharVector) args.get(0);
+      org.apache.arrow.vector.VarCharVector right = (org.apache.arrow.vector.VarCharVector) args.get(1);
+      org.apache.arrow.vector.VarCharVector out =
+          new org.apache.arrow.vector.VarCharVector("concat_out", allocator);
+      int n = left.getValueCount();
+      out.allocateNew(n);
+      for (int i = 0; i < n; i++) {
+        if (left.isNull(i) || right.isNull(i)) {
+          out.setNull(i);
+        } else {
+          byte[] l = left.get(i);
+          byte[] r = right.get(i);
+          byte[] both = new byte[l.length + r.length];
+          System.arraycopy(l, 0, both, 0, l.length);
+          System.arraycopy(r, 0, both, l.length, r.length);
+          out.setSafe(i, both);
+        }
+      }
+      out.setValueCount(n);
+      return out;
+    }
+  }
+
+  @Test
+  void concat_overVarCharColumns_concatenatesValues() throws Exception {
+    try (SessionContext ctx = new SessionContext();
+        BufferAllocator allocator = new RootAllocator()) {
+      ctx.registerUdf(
+          "java_concat",
+          new Concat(),
+          new ArrowType.Utf8(),
+          List.of(new ArrowType.Utf8(), new ArrowType.Utf8()),
+          Volatility.IMMUTABLE);
+
+      try (DataFrame df =
+              ctx.sql(
+                  "SELECT java_concat(a, b) AS c FROM (VALUES ('foo','bar'),('hello','!')) AS t(a, b)");
+          ArrowReader r = df.collect(allocator)) {
+        assertEquals(true, r.loadNextBatch());
+        VectorSchemaRoot root = r.getVectorSchemaRoot();
+        org.apache.arrow.vector.VarCharVector c =
+            (org.apache.arrow.vector.VarCharVector) root.getVector("c");
+        assertEquals(2, c.getValueCount());
+        assertEquals("foobar", new String(c.get(0)));
+        assertEquals("hello!", new String(c.get(1)));
+      }
+    }
+  }
+
+  /** Squares a Float64 column. */
+  static final class Square implements ScalarUdf {
+    @Override
+    public FieldVector evaluate(BufferAllocator allocator, List<FieldVector> args) {
+      org.apache.arrow.vector.Float8Vector in = (org.apache.arrow.vector.Float8Vector) args.get(0);
+      org.apache.arrow.vector.Float8Vector out =
+          new org.apache.arrow.vector.Float8Vector("square_out", allocator);
+      int n = in.getValueCount();
+      out.allocateNew(n);
+      for (int i = 0; i < n; i++) {
+        if (in.isNull(i)) {
+          out.setNull(i);
+        } else {
+          double v = in.get(i);
+          out.set(i, v * v);
+        }
+      }
+      out.setValueCount(n);
+      return out;
+    }
+  }
+
+  @Test
+  void square_overFloat64Column_squaresValues() throws Exception {
+    try (SessionContext ctx = new SessionContext();
+        BufferAllocator allocator = new RootAllocator()) {
+      ctx.registerUdf(
+          "java_square",
+          new Square(),
+          new ArrowType.FloatingPoint(org.apache.arrow.vector.types.FloatingPointPrecision.DOUBLE),
+          List.of(
+              new ArrowType.FloatingPoint(
+                  org.apache.arrow.vector.types.FloatingPointPrecision.DOUBLE)),
+          Volatility.IMMUTABLE);
+
+      try (DataFrame df =
+              ctx.sql("SELECT java_square(x) AS y FROM (VALUES (2.0),(3.5)) AS t(x)");
+          ArrowReader r = df.collect(allocator)) {
+        assertEquals(true, r.loadNextBatch());
+        VectorSchemaRoot root = r.getVectorSchemaRoot();
+        org.apache.arrow.vector.Float8Vector y =
+            (org.apache.arrow.vector.Float8Vector) root.getVector("y");
+        assertEquals(2, y.getValueCount());
+        assertEquals(4.0, y.get(0), 0.0);
+        assertEquals(12.25, y.get(1), 0.0);
+      }
+    }
+  }
+
+  @Test
+  void addOne_invokedTwiceInOneSession_executesIndependently() throws Exception {
+    // Re-running the same UDF query twice exercises that GlobalRefs and JNI state
+    // don't accumulate across invocations within a session.
+    try (SessionContext ctx = new SessionContext();
+        BufferAllocator allocator = new RootAllocator()) {
+      ctx.registerUdf(
+          "add_one",
+          new AddOne(),
+          new ArrowType.Int(32, true),
+          List.of(new ArrowType.Int(32, true)),
+          Volatility.IMMUTABLE);
+
+      for (int run = 0; run < 2; run++) {
+        try (DataFrame df = ctx.sql("SELECT add_one(CAST(5 AS INT)) AS y");
+            ArrowReader r = df.collect(allocator)) {
+          assertEquals(true, r.loadNextBatch());
+          IntVector y = (IntVector) r.getVectorSchemaRoot().getVector("y");
+          assertEquals(1, y.getValueCount());
+          assertEquals(6, y.get(0));
+        }
+      }
+    }
+  }
 }
